42d1389d62d8ba876f6359432c9292b1182f011f
binitworld <contactbinitbhushan@gmail.com>
2023-09-01 09:57:49 +0530

    lab cicd

---
+++ addingtwono.java
@@ -0,0 +1,13 @@
+public class addingtwono {
+    public static void main(String[] args) {
+        // Declare two variables to hold the numbers
+        int num1 = 5;
+        int num2 = 10;
+        
+        // Perform addition
+        int sum = num1 + num2;
+        
+        // Display the result
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
--- first.java
+++ first.java
@@ -1,5 +1,5 @@
 class first{  
     public static void main(String args[]){  
-     System.out.println("Hello everyone this is Binit!");  
+    System.out.println("Hello everyone this is Binit!");  
     }  
 }  
